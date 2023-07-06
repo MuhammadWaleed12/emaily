9e02c32ad182978de96ebc06c78b57e3b05f8fa7
MuhammadWaleedPlastk <waleed@plastk.ca>
2023-07-06 11:02:29 +0500

    google-teak

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ authRoutes(app);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on Port${PORT}`);
+  console.log(`Server is running on Port ${PORT}`);
 });
