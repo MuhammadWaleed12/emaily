f3c89789b9704cf26c168210fa88be7bdcc72826
MuhammadWaleedPlastk <waleed@plastk.ca>
2023-07-06 13:01:35 +0500

    github

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   //we are dev return prod keys
-  module.exports = require('./dev');
+  module.exports = require('./prod');
 } else {
   //we are in production return dev keys
-  module.exports = require('./prod');
+  module.exports = require('./dev');
 }
